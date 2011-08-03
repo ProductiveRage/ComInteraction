e405de5aa2198971a60f6443d972f2a9edf9540f
Dan Roberts <dangger36@gmail.com>
2011-08-03 21:48:00 +0100

    - Split up InterfaceApplierFactory to make it more readable - Added a unit test for translating a property type - Altered note in README relating to possible use of "dynamic"

---
+++ COMInteraction/Misc/InterfaceHierarchyCombiner.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace COMInteraction.Misc
+{
+    public class InterfaceHierarchyCombiner
+    {
+        private Type _targetInterface;
+        private NonNullImmutableList<Type> _interfaces;
+        public InterfaceHierarchyCombiner(Type targetInterface)
+        {
+            if (targetInterface == null)
+                throw new ArgumentNullException("targetInterface");
+            if (!targetInterface.IsInterface)
+                throw new ArgumentException("targetInterface must be an interface type", "targetInterface");
+
+            var interfaces = new List<Type>();
+            buildInterfaceInheritanceList(targetInterface, interfaces);
+            _interfaces = new NonNullImmutableList<Type>(interfaces);
+            _targetInterface = targetInterface;
+        }
+
+        private static void buildInterfaceInheritanceList(Type targetInterface, List<Type> types)
+        {
+            if (targetInterface == null)
+                throw new ArgumentNullException("targetInterface");
+            if (!targetInterface.IsInterface)
+                throw new ArgumentException("targetInterface must be an interface type", "targetInterface");
+            if (types == null)
+                throw new ArgumentNullException("types");
+            
+            if (!types.Contains(targetInterface))
+                types.Add(targetInterface);
+            
+            foreach (var inheritedInterface in targetInterface.GetInterfaces())
+            {
+                if (!types.Contains(inheritedInterface))
+                {
+                    types.Add(inheritedInterface);
+                    buildInterfaceInheritanceList(inheritedInterface, types);
+                }
+            }
+        }
+
+        public Type TargetInterface
+        {
+            get { return _targetInterface; }
+        }
+        
+        public NonNullImmutableList<Type> Interfaces
+        {
+            get { return _interfaces; }
+        }
+    }
+}
